933bc69a9c42ac78cb9ba18bd3cdb11315bb0cbe
smhrd <smhrd@211.228.63.245>
2022-04-15 16:36:51 +0900

    영광_다섯번째 커밋

--- GitTest/src/main/java/Ex01.java
+++ GitTest/src/main/java/Ex01.java
@@ -5,13 +5,12 @@ public class Ex01 {
 
 		System.out.println("첫번째 커밋");
 		System.out.println("두번째 커밋");
+		System.out.println("세번째 커밋");
 		
 		for(int i =0; i<10;i++) {
 			System.out.println("세번째 커밋");
 		}
-		
-		
-		
+		System.out.println("네번째 커밋");
 		
 	}
 
