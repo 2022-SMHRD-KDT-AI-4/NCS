7c2ff2671296e46356a4ac05eb7a3a327cb5ef45
smhrd <smhrd@125.136.66.81>
2022-04-15 16:33:31 +0900

    다섯번째_민지 커밋

--- GitTest/src/main/java/Ex01.java
+++ GitTest/src/main/java/Ex01.java
@@ -5,7 +5,7 @@ public class Ex01 {
 
 		System.out.println("첫번째 커밋");
 		System.out.println("두번째 커밋");
-		System.out.println("세번째 커밋");
+		System.out.println("다섯번째 커밋");
 		
 		for(int i =0; i<10;i++) {
 			System.out.println("세번째 커밋");
